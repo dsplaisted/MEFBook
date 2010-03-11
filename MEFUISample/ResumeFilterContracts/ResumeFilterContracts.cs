f58a1f0350e47f04ef9748615b6781ebc0b81757
Daniel Plaisted <dsplaisted@gmail.com>
2010-03-10 21:36:03 -0800

    Initial Resume class

--- MEFUISample/ResumeFilterContracts/ResumeFilterContracts.cs
+++ MEFUISample/ResumeFilterContracts/ResumeFilterContracts.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace ResumeFilterContracts
 {
-	public class ResumeFilterContracts
+	public class Resume
 	{
+		public Dictionary<string, Proficiency> Skills { get; set; }
+		public List<EmploymentHistoryItem> EmploymentHistory { get; set; }
+		public List<EducationHistoryItem> EducationHistory { get; set; }
+	}
+
+	public enum Proficiency
+	{
+		None,
+		Beginner,
+		Intermediate,
+		Advanced,
+		Expert
+	}
+
+	public class EmploymentHistoryItem
+	{
+		public string Company { get; set; }
+		public string Position { get; set; }
+		public double YearsEmployed { get; set; }
+	}
+
+	public class EducationHistoryItem
+	{
+		public string InstitutionName { get; set; }
+		public string DegreeSubject { get; set; }				//	Computer science, Philosophy, etc
+		public string DegreeLevel { get; set; }					//	B.S., M.S., PhD
+		public int YearGraduated { get; set; }
 	}
 }
